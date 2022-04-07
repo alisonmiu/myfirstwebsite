7aa12f687c253953775d0105287164006be1c3f7
Alison Miu <alisonmiu@gmail.com>
2022-04-07 20:56:40 +1000

    Lightbox updates

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+window.document.onkeydown = function(e) {
+	if (!e) {
+		e = event;
+	}
+	if (e.keyCode == 27) {
+		lightbox_close();
+	}
+}
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-	image.addEventListener('click', e => {
-		lightbox.classList.add('active')
-		const img = document.createElement('img')
-		img.src = image.getAttribute(data-trailer)
-		while (lightbox.firstChild) {
-			lightbox.removeChild(lightbox.firstChild)
-		}
-		lightbox.appendChild(img)
+// Get all film images
+let films = document.querySelectorAll(".films img");
+
+// On click, show the lightbox with video
+films.forEach(function(image) {
+	image.addEventListener('click', function(event) {
+		let video_url = event.target.getAttribute('data-trailer')
+		let embed = youtube_embed(video_url)
+		let player = document.getElementById('lightbox-player')
+		player.innerHTML = '';
+		player.appendChild(embed)
+
+		document.getElementById('light').style.display = 'block';
 	})
 })
 
-lightbox.addEventListener('click', e => {
-	if (e.target !== e.currentTarget) return
-	lightbox.classList.remove('active')
-})
+function youtube_embed(url) {
+	let iframe = '<iframe width="560" height="315" src="' + url + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+	let div = document.createElement('div')
+	div.innerHTML = iframe
+	return div
+}
+
+function lightbox_open() {
+	let lightBoxVideo = document.getElementById("MorbiusVideo");
+	window.scrollTo(0, 0);
+
+	document.getElementById('light').style.display = 'block';
+
+	document.getElementById('fade').style.display = 'block';
+	lightBoxVideo.play();
+}
+
+function lightbox_close() {
+	let lightBoxVideo = document.getElementById("MorbiusVideo");
+
+	document.getElementById('light').style.display = 'none';
+
+	document.getElementById('fade').style.display = 'none';
+	lightBoxVideo.pause();
+}
